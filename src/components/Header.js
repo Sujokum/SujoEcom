4c75c497744e25a856aa5c36b1818066fa81b0a1
Sujokum <suneelahmed413@gmail.com>
2022-12-28 13:14:42 +0500

    added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from 'styled-components';
-import {NavLink} from 'react-router-dom'
+import {NavLink} from 'react-router-dom';
+import Navbar from './Navbar';
 const Header = () => {
   return (
     <MainHeader>
       <NavLink to={'/'} >
-        <img src="https://www.freepnglogos.com/uploads/company-logo-png/company-logo-transparent-png-19.png" alt="logo" />
-      </NavLink> 
+        <img className='logo' src="https://www.freepnglogos.com/uploads/company-logo-png/company-logo-transparent-png-19.png" alt="logo" />
+      </NavLink>
+      <Navbar/> 
     </MainHeader>
   )
 }
@@ -15,6 +17,7 @@ const MainHeader = styled.header`
 height: 10rem;
 background-color:  ${({theme})=> theme.colors.bg };
 display: flex;
+padding: 0rem 1.5rem;
 justify-content: space-between;
 align-items: center;
 position: relative;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,87 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Nav>
+      <div className='InnerMainNav' >
+        <ul>
+            <li>
+              <NavLink to={'/'} >
+                        Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={'/about'} >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={'/products'} >
+                Products
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={'/contact'} >
+              Contact
+              </NavLink>
+            </li>
+            <li className={'btn'} >
+              <NavLink   to={'/login'} >
+                  Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={'cart'} to={'/cart'}>
+              <AiOutlineShoppingCart/>
+              </NavLink>
+            </li>
+        </ul>
+      </div>
+      </Nav>
   )
 }
+
+const Nav = styled.nav`
+height: 100%;
+width: 30%;
+.InnerMainNav{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content : center;
+  align-items: center;
+}
+.InnerMainNav ul {
+  display: inline-flex;
+  width: 100%;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+}
+.InnerMainNav ul li{
+  font-size: 2rem;
+}
+.InnerMainNav ul li:hover{
+  text-shadow: 1px 1px 2px black;
+  cursor: pointer;
+}
+.btn{
+  width: 20% ;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 2rem;
+  
+
+  background-color: aqua;
+}
+.cart{
+  font-size: 2.5rem;
+}
+`
 
 export default Navbar
